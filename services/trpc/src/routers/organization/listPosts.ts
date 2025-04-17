7bd705d5f87e923db9d8260049c0000984a16966
Scott Cazan <scottcazan@gmail.com>
2025-04-17 12:35:05 +0200

    fix openapi definition

--- services/trpc/src/routers/organization/listPosts.ts
+++ services/trpc/src/routers/organization/listPosts.ts
@@ -16,7 +16,7 @@ const meta: OpenApiMeta = {
   openapi: {
     enabled: true,
     method: 'GET',
-    path: '/organization/{organization}/feed/posts',
+    path: '/organization/{slug}/feed/posts',
     protect: true,
     tags: ['organization'],
     summary: 'List posts for an organization',
