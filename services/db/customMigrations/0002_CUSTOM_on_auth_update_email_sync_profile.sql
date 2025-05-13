0b2ac6bcd4ef612ed8448a0d72cdf11735e35926
Scott Cazan <scottcazan@gmail.com>
2025-05-13 17:12:21 +0200

    cleanup migrations

--- services/db/customMigrations/0002_CUSTOM_on_auth_update_email_sync_profile.sql
+++ services/db/customMigrations/0002_CUSTOM_on_auth_update_email_sync_profile.sql
@@ -1,7 +1,7 @@
 -- Custom SQL migration file, put you code below! --
 CREATE
-OR REPLACE FUNCTION public.sync_profile_email () RETURNS trigger LANGUAGE plpgsql SECURITY DEFINER AS $function$begin
-  update public.profiles
+OR REPLACE FUNCTION public.sync_user_email () RETURNS trigger LANGUAGE plpgsql SECURITY DEFINER AS $function$begin
+  update public.users
   set email = new.email,
       updated_at = new.updated_at
   where id = new.id;
@@ -9,10 +9,10 @@ OR REPLACE FUNCTION public.sync_profile_email () RETURNS trigger LANGUAGE plpgsq
 end;$function$;
 
 DO $$ BEGIN
-  CREATE TRIGGER on_auth_update_email_sync_profile
+  CREATE TRIGGER on_auth_update_email_sync_user
   AFTER
   UPDATE OF email ON auth.users FOR EACH ROW
-  EXECUTE FUNCTION sync_profile_email ();
+  EXECUTE FUNCTION sync_user_email ();
 EXCEPTION
   WHEN duplicate_object THEN null;
 END $$;
