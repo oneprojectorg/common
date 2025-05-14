e95c93b200afef345b93f924db0ea44bb8caa6e0
Scott Cazan <scottcazan@gmail.com>
2025-05-14 18:33:53 +0200

    update drizzle

--- apps/app/src/app/layout.tsx
+++ apps/app/src/app/layout.tsx
@@ -3,6 +3,7 @@ import { createTRPCNextClient } from '@op/api/next';
 import { APP_NAME, printNFO } from '@op/core';
 import { Toast } from '@op/ui/Toast';
 import '@op/ui/tailwind-styles';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import type { Metadata, Viewport } from 'next';
 import { Inter, Roboto, Roboto_Mono, Roboto_Serif } from 'next/font/google';
 import { redirect } from 'next/navigation';
@@ -92,6 +93,7 @@ const RootLayout = async ({ children }: { children: React.ReactNode }) => {
           className={`${roboto.variable} ${robotoMono.variable} ${robotoSerif.variable} ${inter.variable} overflow-x-hidden text-base text-neutral-black antialiased`}
         >
           <PostHogProvider>{children}</PostHogProvider>
+          <ReactQueryDevtools initialIsOpen={false} />
           <Toast />
         </body>
       </TRPCProvider>
