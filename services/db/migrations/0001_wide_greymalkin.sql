2f9e4b45d5301a3f3c8c8feaf749f4b5a33c4f0d
Scott Cazan <scottcazan@gmail.com>
2025-05-14 18:10:41 +0200

    messing with indexes in drizzle

--- services/db/migrations/0001_wide_greymalkin.sql
+++ services/db/migrations/0001_wide_greymalkin.sql
@@ -1,7 +1,2 @@
 ALTER TABLE "organizations" RENAME COLUMN "description" TO "bio";--> statement-breakpoint
-COMMIT;--> statement-breakpoint
-CREATE INDEX CONCURRENTLY "links_organization_id_idx" ON "links" USING btree ("organization_id");--> statement-breakpoint
-CREATE INDEX CONCURRENTLY "organizationUsers_auth_user_id_idx" ON "organization_users" USING btree ("auth_user_id");--> statement-breakpoint
-CREATE INDEX CONCURRENTLY "organizations_header_image_id_idx" ON "organizations" USING btree ("header_image_id");--> statement-breakpoint
-CREATE INDEX CONCURRENTLY "organizations_avatar_image_id_idx" ON "organizations" USING btree ("avatar_image_id");--> statement-breakpoint
-CREATE INDEX CONCURRENTLY "projects_organization_id_idx" ON "projects" USING btree ("organization_id");
+
