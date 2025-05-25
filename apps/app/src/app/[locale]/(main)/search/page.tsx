6bea4261efdecce967c5d4728326cde552c5ab0b
Scott Cazan <scottcazan@gmail.com>
2025-05-25 22:57:34 +0200

    setting layout

--- apps/app/src/app/[locale]/(main)/search/page.tsx
+++ apps/app/src/app/[locale]/(main)/search/page.tsx
@@ -7,7 +7,16 @@ const SearchListingPage = async ({
 }) => {
   const { q = '' } = await searchParams;
 
-  return <OrganizationSearchResults query={q} />;
+  return (
+    <div className="flex w-full flex-col gap-3 pt-8 sm:min-h-[calc(100vh-3.5rem)] sm:gap-6">
+      <div className="flex flex-col gap-4 px-4 sm:px-0">
+        <div className="font-serif text-title-lg text-neutral-gray4">
+          Results for <span className="text-neutral-black">{q}</span>
+        </div>
+      </div>
+      <OrganizationSearchResults query={q} />
+    </div>
+  );
 };
 
 export default SearchListingPage;
