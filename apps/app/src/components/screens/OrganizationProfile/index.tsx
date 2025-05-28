c803390bdb7bc3c0d166309394a07070ab4d2427
Scott Cazan <scottcazan@gmail.com>
2025-05-28 23:50:57 +0200

    more SSR

--- apps/app/src/components/screens/OrganizationProfile/index.tsx
+++ apps/app/src/components/screens/OrganizationProfile/index.tsx
@@ -1,9 +1,7 @@
 import { getPublicUrl } from '@/utils';
 import { trpcNext } from '@op/api/vanilla';
-import { Skeleton } from '@op/ui/Skeleton';
 import { cn, getGradientForString } from '@op/ui/utils';
 import Image from 'next/image';
-import React, { Suspense } from 'react';
 import { LuArrowLeft } from 'react-icons/lu';
 
 import { Link } from '@/lib/i18n';
@@ -13,44 +11,57 @@ import { ProfileGrid, ProfileTabs } from '@/components/Profile/ProfileContent';
 import { ProfileDetails } from '@/components/Profile/ProfileDetails';
 
 const OrganizationProfileSuspense = async ({ slug }: { slug: string }) => {
-  const client = await trpcNext();
-  const organization = await client.organization.getBySlug.query({
-    slug,
-  });
+  try {
+    const client = await trpcNext();
+    const organization = await client.organization.getBySlug.query({
+      slug,
+    });
 
-  const { headerImage, avatarImage } = organization;
-  const headerUrl = getPublicUrl(headerImage?.name);
-  const avatarUrl = getPublicUrl(avatarImage?.name);
+    const { headerImage, avatarImage } = organization;
+    const headerUrl = getPublicUrl(headerImage?.name);
+    const avatarUrl = getPublicUrl(avatarImage?.name);
 
-  const gradientBg = getGradientForString(organization.name || 'Common');
-  const gradientBgHeader = getGradientForString(
-    organization.name + 'C' || 'Common',
-  );
+    const gradientBg = getGradientForString(organization.name || 'Common');
+    const gradientBgHeader = getGradientForString(
+      organization.name + 'C' || 'Common',
+    );
 
-  return (
-    <>
+    return (
+      <>
+        <ImageHeader
+          headerImage={
+            headerUrl ? (
+              <Image src={headerUrl} alt="" fill className="object-cover" />
+            ) : (
+              <div className={cn('h-full w-full', gradientBgHeader)} />
+            )
+          }
+          avatarImage={
+            avatarUrl ? (
+              <Image src={avatarUrl} alt="" fill className="object-cover" />
+            ) : (
+              <div className={cn('h-full w-full', gradientBg)} />
+            )
+          }
+        />
+
+        <ProfileDetails profile={organization} />
+        <ProfileGrid profile={organization} />
+        <ProfileTabs profile={organization} />
+      </>
+    );
+  } catch (e) {
+    console.error(e);
+    return (
       <ImageHeader
         headerImage={
-          headerUrl ? (
-            <Image src={headerUrl} alt="" fill className="object-cover" />
-          ) : (
-            <div className={cn('h-full w-full', gradientBgHeader)} />
-          )
-        }
-        avatarImage={
-          avatarUrl ? (
-            <Image src={avatarUrl} alt="" fill className="object-cover" />
-          ) : (
-            <div className={cn('h-full w-full', gradientBg)} />
-          )
+          <div className="flex h-full w-full items-center justify-center">
+            Could not load organization
+          </div>
         }
       />
-
-      <ProfileDetails profile={organization} />
-      <ProfileGrid profile={organization} />
-      <ProfileTabs profile={organization} />
-    </>
-  );
+    );
+  }
 };
 
 export const OrganizationProfile = ({ slug }: { slug: string }) => {
@@ -63,9 +74,7 @@ export const OrganizationProfile = ({ slug }: { slug: string }) => {
         </Link>
       </header>
       <div className="-mt-12 flex w-full flex-col gap-3 border-offWhite border-b-transparent sm:mt-0 sm:min-h-[calc(100vh-3.5rem)] sm:gap-4 sm:border">
-        <Suspense fallback={<Skeleton className="h-96 w-full" />}>
-          <OrganizationProfileSuspense slug={slug} />
-        </Suspense>
+        <OrganizationProfileSuspense slug={slug} />
       </div>
     </>
   );
