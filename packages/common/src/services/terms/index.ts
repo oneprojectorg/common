f563bbbdc1ff20e89caa5545bfb934677b2e7f5d
Scott Cazan <scottcazan@gmail.com>
2025-06-17 21:52:16 +0200

    fix issue in terms lookup

--- packages/common/src/services/terms/index.ts
+++ packages/common/src/services/terms/index.ts
@@ -6,98 +6,6 @@ interface TermLookupHandler {
   name: string;
 }
 
-/*
-const getTermsFromCandid = async ({ query, name }: TermLookupHandler) => {
-  const [, facet] = name.trim().split(':');
-  const gqlQuery = `
-  query termsByFacet($facet: FacetType!) {
-    termsByFacet(facet: $facet) {
-      meta {
-    code
-    message
-    resultsCount
-    took
-  }
-    data {
-    code
-    name
-    description
-    facet
-    depth
-    hasChildren
-  }
-    }
-  }
-  `;
-
-  // const gqlQuery = `
-  // query {
-  // termsByMatch(match: {
-  // searchTerm: "${query?.trim() ?? ''}",
-  // facet: ${facet},
-  // limit: 100}) {
-  // meta {
-  // code
-  // message
-  // resultsCount
-  // took
-  // }
-  // data {
-  // code
-  // name
-  // description
-  // facet
-  // depth
-  // hasChildren
-  // }
-  // }
-  // }
-  // `;
-
-  if (!process.env.CANDID_API_KEY) {
-    return [];
-  }
-
-  const response = await fetch('https://api.candid.org/taxonomy/graphql/', {
-    method: 'POST',
-    headers: new Headers({
-      'Content-Type': 'application/json',
-      'Subscription-Key': process.env.CANDID_API_KEY,
-    }),
-    body: JSON.stringify({
-      query: gqlQuery,
-      variables: { facet },
-    }),
-  });
-
-  const data = await response.json();
-
-  if (data.errors) {
-    console.error('Candid Errors:', data.errors);
-    return null;
-  }
-
-  type CandidTerm = {
-    code: string;
-    name: string;
-    description: string;
-    facet: string;
-    depth: number;
-    hasChildren: boolean;
-  };
-
-  const transformed = data.data.termsByFacet.data.map((term: CandidTerm) => ({
-    id: term.code,
-    label: term.name,
-    taxonomyId: name,
-    definition: term.description,
-    data: term,
-  }));
-
-  return transformed;
-};
-*/
-
 const getTermsFromDb = async ({
   query,
   name,
@@ -120,18 +28,23 @@ const getTermsFromDb = async ({
   }
 
   if (query) {
-    whereClause = sql`${taxonomies.namespaceUri} = ${taxonomyName} AND ${taxonomyTerms.facet} = ${facet?.trim() ?? null} AND ${taxonomyTerms.label} @@to_tsquery('english', ${query + ':*'})`;
+    whereClause = sql`${taxonomies.namespaceUri} = ${taxonomyName} AND ${taxonomyTerms.facet} ${facet ? sql`= ${facet.trim()}` : sql`IS NULL`} AND ${taxonomyTerms.label} @@to_tsquery('english', ${query + ':*'})`;
   }
 
-  const results = await db
-    .select()
-    .from(taxonomyTerms)
-    .innerJoin(taxonomies, () => eq(taxonomyTerms.taxonomyId, taxonomies.id))
-    .where(whereClause);
+  try {
+    const results = await db
+      .select()
+      .from(taxonomyTerms)
+      .innerJoin(taxonomies, () => eq(taxonomyTerms.taxonomyId, taxonomies.id))
+      .where(whereClause);
 
-  const terms = results.map((row) => row.taxonomyTerms);
+    const terms = results.map((row) => row.taxonomyTerms);
 
-  return terms;
+    return terms;
+  } catch (e) {
+    console.error(e);
+    throw e;
+  }
 };
 
 const nameHandlers: Record<
