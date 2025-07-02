b0e45ce50d6533041cae0831ae871b2ec53d2901
Scott Cazan <scottcazan@gmail.com>
2025-07-02 11:12:13 +0200

    update tos short

--- apps/app/src/components/ToSContent/ToSContentShort.tsx
+++ apps/app/src/components/ToSContent/ToSContentShort.tsx
@@ -12,8 +12,8 @@ export const ToSContentShort = () => {
         </p>
       </FormalSection>
       <FormalSection>
-        <Header3 className="font-serif">1. Who We Are</Header3>
-        <p>
+        <Header3 className="font-serif">Who We Are</Header3>
+        <p className="mb-4">
           Common is a digital platform that connects people, organizations, and
           resources to coordinate and grow economic democracy to global scale.
           The Common platform is run by One Project. One Project is a nonprofit
@@ -21,14 +21,21 @@ export const ToSContentShort = () => {
           people and planet, not profit. We partner with the rising global
           movement for economic democracy to build core technology, mobilize
           catalytic resources, and show the world that a better system is
-          possible today.
+          possible today. You can see the full legal version of our terms of use
+          <a href="/info/tos" target="_blank">
+            here
+          </a>
+          .
         </p>
       </FormalSection>
 
       <FormalSection>
-        <Header3 className="font-serif">2. Welcome to Common</Header3>
-
-        <p>
+        <Header3 className="font-serif">Welcome to Common</Header3>
+        <p className="mb-4">
+          This is a summarized version of our Terms of Use. Please read the full
+          legal version in the dropdown below for complete details.
+        </p>
+        <p className="mb-4">
           These terms are an agreement between you and One Project that govern
           your use of our website (oneproject.org) and the Common platform.
           Common is a digital platform that connects people, organizations, and
@@ -38,8 +45,8 @@ export const ToSContentShort = () => {
       </FormalSection>
 
       <FormalSection>
-        <Header3 className="font-serif">3. Who Can Use Common</Header3>
-        <p>
+        <Header3 className="font-serif">Who Can Use Common</Header3>
+        <p className="mb-4">
           You must be at least 18 years old to use Common. We may require you to
           meet certain criteria to use the platform, and Common is currently
           invite-only.
@@ -47,8 +54,8 @@ export const ToSContentShort = () => {
       </FormalSection>
 
       <FormalSection>
-        <Header3 className="font-serif">4. Your Account</Header3>
-        <p>
+        <Header3 className="font-serif">Your Account</Header3>
+        <p className="mb-4">
           When creating your account, provide accurate information and keep your
           password secure. Don't use fake details or pretend to be someone else.
           You're responsible for everything that happens on your account and it
@@ -61,57 +68,59 @@ export const ToSContentShort = () => {
       </FormalSection>
 
       <FormalSection>
-        <Header3 className="font-serif">5. Our Code of Conduct</Header3>
-        <p>
+        <Header3 className="font-serif">Our Code of Conduct</Header3>
+        <p className="mb-4">
           All users of the Platform must comply with our Code of Conduct,
           including these core community principles:
         </p>
-        <p>
-          <strong>Treat Everyone with Respect and Consideration</strong>{' '}
-          (adopted from DisCO Caring Community Guidelines): Be kind,
-          understanding, and compassionate to each other. Be welcoming. Be aware
-          of how we use language. This is especially important when discussing
-          sensitive topics where misunderstandings and stereotypes can be
-          exacerbated. Remember we're here to support, learn, and collaborate
-          with each other to grow a democratic economy.
-        </p>
-        <p>
-          <strong>Be Respectful of Privacy:</strong> Respect the privacy and
-          confidentiality of others. If there are sensitive partnerships, obtain
-          permission before making them visible on the Platform.
-        </p>
-        <p>
-          <strong>Build Robust Relationships:</strong> Build strong, resilient
-          connections that strengthen collaboration, deepen existing
-          partnerships, and enable new relationships to emerge.
-        </p>
-        <p>
-          <strong>Inclusive Network Development:</strong> Wherever possible, try
-          to amplify the voice and needs of smaller local organizations that may
-          initially have limited connections as they start to engage new
-          collaborations on the Platform.
-        </p>
-        <p>
-          <strong>Center Affected Communities</strong> (adopted from New Economy
-          Coalition Agreements): Recognize and respect the expertise,
-          leadership, and lived experiences of communities most impacted by
-          economic inequality. Actively make space for their voices, defer to
-          their guidance on issues affecting them directly, and acknowledge
-          their essential role in developing meaningful solutions.
-        </p>
-        <p>
-          <strong>Share from Your Own Experiences</strong> (adopted from New
-          Economy Coalition Agreements): Be sure to share information and
-          perspectives from your experiences rather than making assumptions
-          about or speaking on behalf of others. When sharing information,
-          clearly distinguish among personal knowledge, organizational
-          position/perspective, and third-party information.
-        </p>
+        <ul className="mb-4 list-disc pl-6">
+          <li className="mb-2">
+            <strong>Treat Everyone with Respect and Consideration</strong>{' '}
+            (adopted from DisCO Caring Community Guidelines): Be kind,
+            understanding, and compassionate to each other. Be welcoming. Be
+            aware of how we use language. This is especially important when
+            discussing sensitive topics where misunderstandings and stereotypes
+            can be exacerbated. Remember we're here to support, learn, and
+            collaborate with each other to grow a democratic economy.
+          </li>
+          <li className="mb-2">
+            <strong>Be Respectful of Privacy:</strong> Respect the privacy and
+            confidentiality of others. If there are sensitive partnerships,
+            obtain permission before making them visible on the Platform.
+          </li>
+          <li className="mb-2">
+            <strong>Build Robust Relationships:</strong> Build strong, resilient
+            connections that strengthen collaboration, deepen existing
+            partnerships, and enable new relationships to emerge.
+          </li>
+          <li className="mb-2">
+            <strong>Inclusive Network Development:</strong> Wherever possible,
+            try to amplify the voice and needs of smaller local organizations
+            that may initially have limited connections as they start to engage
+            new collaborations on the Platform.
+          </li>
+          <li className="mb-2">
+            <strong>Center Affected Communities</strong> (adopted from New
+            Economy Coalition Agreements): Recognize and respect the expertise,
+            leadership, and lived experiences of communities most impacted by
+            economic inequality. Actively make space for their voices, defer to
+            their guidance on issues affecting them directly, and acknowledge
+            their essential role in developing meaningful solutions.
+          </li>
+          <li className="mb-2">
+            <strong>Share from Your Own Experiences</strong> (adopted from New
+            Economy Coalition Agreements): Be sure to share information and
+            perspectives from your experiences rather than making assumptions
+            about or speaking on behalf of others. When sharing information,
+            clearly distinguish among personal knowledge, organizational
+            position/perspective, and third-party information.
+          </li>
+        </ul>
       </FormalSection>
 
       <FormalSection>
-        <Header3 className="font-serif">6. What You Can Do</Header3>
-        <p>
+        <Header3 className="font-serif">What You Can Do</Header3>
+        <p className="mb-4">
           You can use the platform for collaboration and connection, share your
           organization's information and resources, and connect with other users
           in the network.
@@ -119,8 +128,8 @@ export const ToSContentShort = () => {
       </FormalSection>
 
       <FormalSection>
-        <Header3 className="font-serif">7. What You Can't Do</Header3>
-        <p>
+        <Header3 className="font-serif">What You Can't Do</Header3>
+        <p className="mb-4">
           You can't copy, modify, distribute, or create new versions of our
           platform without our written permission. You can't try to reverse
           engineer our code, hack our security systems, or use automated tools
@@ -135,8 +144,8 @@ export const ToSContentShort = () => {
       </FormalSection>
 
       <FormalSection>
-        <Header3 className="font-serif">8. Prohibited Content</Header3>
-        <p>
+        <Header3 className="font-serif">Prohibited Content</Header3>
+        <p className="mb-4">
           You agree to not post content that is abusive, insulting, threatening,
           or discriminatory, or that promotes hatred, racism, sexism, or
           bigotry. You agree to not harass, upset, embarrass, or annoy others,
@@ -150,18 +159,18 @@ export const ToSContentShort = () => {
       </FormalSection>
 
       <FormalSection>
-        <Header3 className="font-serif">9. Your Content</Header3>
-        <p>
+        <Header3 className="font-serif">Your Content</Header3>
+        <p className="mb-4">
           You own the content you post, but by posting, you give us permission
           to display and share it on the platform. Make sure you have the right
           to share everything you post, as we're not responsible for your
-          content.
+          content. We prohibit the following type of content:
         </p>
       </FormalSection>
 
       <FormalSection>
-        <Header3 className="font-serif">10. Our Code</Header3>
-        <p>
+        <Header3 className="font-serif">Our Code</Header3>
+        <p className="mb-4">
           The software code that runs our platform is open source, meaning it's
           freely available for you to use and contribute to under specific
           license terms. We use the Apache 2.0 license modified by the Commons
@@ -173,16 +182,16 @@ export const ToSContentShort = () => {
       </FormalSection>
 
       <FormalSection>
-        <Header3 className="font-serif">11. Ending Your Account</Header3>
-        <p>
+        <Header3 className="font-serif">Ending Your Account</Header3>
+        <p className="mb-4">
           You can stop using the platform anytime, and we may terminate accounts
           that violate these terms.
         </p>
       </FormalSection>
 
       <FormalSection>
-        <Header3 className="font-serif">12. Disputes and Governing Law</Header3>
-        <p>
+        <Header3 className="font-serif">Disputes and Governing Law</Header3>
+        <p className="mb-4">
           These terms of use are governed by the laws of the State of
           California. Most disputes must be resolved through individual
           arbitration, you waive rights to class action lawsuits.
@@ -190,12 +199,8 @@ export const ToSContentShort = () => {
       </FormalSection>
 
       <FormalSection>
-        <Header3 className="font-serif">13. Contact Us</Header3>
-        <p>Questions? Email us at support@oneproject.org</p>
-        <p>
-          Last updated: June 23, 2025. Questions? Contact us at:
-          support@oneproject.org
-        </p>
+        <Header3 className="font-serif">Contact Us</Header3>
+        <p className="mb-4">Questions? Email us at support@oneproject.org</p>
       </FormalSection>
     </div>
   );
