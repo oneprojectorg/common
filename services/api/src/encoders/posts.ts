f41f317fa599286eedb4c07a8b5d13ca1ec7751c
Scott Cazan <scottcazan@gmail.com>
2025-07-24 16:46:41 +0200

    fixing recursive type

--- services/api/src/encoders/posts.ts
+++ services/api/src/encoders/posts.ts
@@ -10,20 +10,28 @@ export const postAttachmentEncoder = createSelectSchema(attachments).extend({
   storageObject: storageItemEncoder,
 });
 
-export const postsEncoder = createSelectSchema(posts)
+const basePostsEncoder = createSelectSchema(posts)
   .extend({
     attachments: z.array(postAttachmentEncoder).default([]),
     reactionCounts: z.record(z.string(), z.number()),
     userReaction: z.string().nullish(),
     commentCount: z.number(),
     profile: profileWithAvatarEncoder.nullish(),
-    // TODO: circular references produce issues in zod so are typed as any for now
-    childPosts: z.array(z.lazy((): z.ZodType<any> => postsEncoder)).nullish(),
-    parentPost: z.lazy((): z.ZodType<any> => postsEncoder).nullish(),
   })
   .strip();
 
-export type Post = z.infer<typeof postsEncoder>;
+// @ts-ignore
+export const postsEncoder: z.ZodType<Post> = basePostsEncoder.extend({
+  childPosts: z.array(z.lazy(() => postsEncoder)).nullish(),
+  parentPost: z.lazy(() => postsEncoder).nullish(),
+});
+
+export type Post = z.infer<typeof basePostsEncoder> & {
+  childPosts: Post[] | null;
+  parentPost?: Post | null;
+};
+
+// export type Post = z.infer<typeof postsEncoder>;
 
 export const postsToOrganizationsEncoder = createSelectSchema(
   postsToOrganizations,
