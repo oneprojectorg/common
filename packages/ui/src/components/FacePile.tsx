1d0dfd048cd9b914cb30589cbbc109e3634cf8a0
Scott Cazan <scottcazan@gmail.com>
2025-08-31 16:39:14 +0200

    Allow wrapping of text and fix ref for resizer

--- packages/ui/src/components/FacePile.tsx
+++ packages/ui/src/components/FacePile.tsx
@@ -1,16 +1,30 @@
 import { ReactNode, forwardRef } from 'react';
 
+import { cn } from '../lib/utils';
+
 export const FacePile = forwardRef<
-  HTMLUListElement,
-  { items: Array<ReactNode> }
->(({ items }: { children?: ReactNode; items: Array<ReactNode> }, ref) => {
-  return (
-    <ul className="-gap-2 flex" ref={ref}>
-      {items.map((node, i) => (
-        <li key={i} className="relative -ml-2">
-          {node}
-        </li>
-      ))}
-    </ul>
-  );
-});
+  HTMLDivElement,
+  { items: Array<ReactNode>; children?: ReactNode; className?: string }
+>(
+  (
+    {
+      items,
+      children,
+      className,
+    }: { className?: string; children?: ReactNode; items: Array<ReactNode> },
+    ref,
+  ) => {
+    return (
+      <div className="flex w-full flex-wrap items-center gap-2" ref={ref}>
+        <ul className={cn('-gap-2 flex', className)}>
+          {items.map((node, i) => (
+            <li key={i} className="relative -ml-2">
+              {node}
+            </li>
+          ))}
+        </ul>
+        {children}
+      </div>
+    );
+  },
+);
