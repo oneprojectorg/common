b6d85c28de1497209a7ad9d36cc444c2e042c02b
Scott Cazan <scottcazan@gmail.com>
2025-09-02 10:26:33 +0200

    add explanatory comment

--- apps/app/src/components/TranslatedText/index.tsx
+++ apps/app/src/components/TranslatedText/index.tsx
@@ -1,7 +1,8 @@
 'use client';
 
 /*
- * Wraps text in the client side translation hook
+ * Wraps text in the client side translation hook for SERVER components that need translated text.
+ * Why: We need to shift to getTranslations but it doesn't yet support our dictionaries so we wrap this here so we can easily shift it out when it does.
  */
 import { useTranslations } from '@/lib/i18n';
 
