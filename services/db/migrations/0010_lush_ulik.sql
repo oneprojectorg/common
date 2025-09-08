5e151eec21aa13fa7c2eabd7450dbd2b8cb6c1f5
Scott Cazan <scottcazan@gmail.com>
2025-09-08 23:30:07 +0200

    update to avoid truncate

--- services/db/migrations/0010_lush_ulik.sql
+++ services/db/migrations/0010_lush_ulik.sql
@@ -1,1 +1,1 @@
-CREATE INDEX "organization_relationships_source_organization_id_target_organization_id_index" ON "organization_relationships" USING btree ("source_organization_id","target_organization_id");
+CREATE INDEX "org_rel_src_org_id_target_org_id_index" ON "organization_relationships" USING btree ("source_organization_id","target_organization_id");
