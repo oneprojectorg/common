72ee61f4dc07819d39d4788eb806f7f684577c54
Scott Cazan <scottcazan@gmail.com>
2025-10-08 05:49:58 +0200

    Fix for initial server auth (cookie race condition) (#202)
    Fixes initial auth issues where we were getting errors.

--- apps/app/src/app/[locale]/(no-header)/layout.tsx
+++ apps/app/src/app/[locale]/(no-header)/layout.tsx
@@ -2,6 +2,8 @@ import { UserProvider } from '@/utils/UserProvider';
 import { trpcNext } from '@op/api/vanilla';
 import { redirect } from 'next/navigation';
 
+export const dynamic = 'force-dynamic';
+
 const Layout = async ({ children }: { children: React.ReactNode }) => {
   const client = await trpcNext();
   const user = await client.account.getMyAccount.query();
