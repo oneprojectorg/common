4f841e5ede6be7d67c5942d5d566c6dbf660dd5f
Valentino Hudhra <v.hudhra@gmail.com>
2025-11-05 11:06:55 +0100

    bring back api for backwards compatibility

---
+++ services/api/src/routers/organization/stats.ts
@@ -0,0 +1,44 @@
+import { getPlatformStats } from '@op/common';
+import { OpenApiMeta } from 'trpc-to-openapi';
+import { z } from 'zod';
+
+import withAnalytics from '../../middlewares/withAnalytics';
+import withAuthenticated from '../../middlewares/withAuthenticated';
+import withRateLimited from '../../middlewares/withRateLimited';
+import { loggedProcedure, router } from '../../trpcFactory';
+
+const meta: OpenApiMeta = {
+  openapi: {
+    enabled: true,
+    method: 'GET',
+    path: '/organization/stats',
+    protect: true,
+    tags: ['organization'],
+    summary: 'Get organization statistics',
+  },
+};
+
+/**
+ * @deprecated Kept to maintain backward compatibility.
+ * TODO: remove!
+ */
+export const organizationStatsRouter = router({
+  getStats: loggedProcedure
+    .use(withRateLimited({ windowSize: 10, maxRequests: 10 }))
+    .use(withAuthenticated)
+    .use(withAnalytics)
+    .meta(meta)
+    .input(z.void())
+    .output(
+      z.object({
+        totalOrganizations: z.number(),
+        totalRelationships: z.number(),
+        newOrganizations: z.number(),
+        totalUsers: z.number(),
+      }),
+    )
+    .query(async ({ ctx }) => {
+      const { user } = ctx;
+      return await getPlatformStats({ user });
+    }),
+});
