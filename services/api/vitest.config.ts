6b189150d54173f2142013346f17f7754a78eb5e
Valentino Hudhra <v.hudhra@gmail.com>
2025-11-13 14:01:00 +0100

    clean up 2

--- services/api/vitest.config.ts
+++ services/api/vitest.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
   test: {
-    maxConcurrency: 3,
+    maxConcurrency: 8,
     environment: 'node',
     globals: true,
     setupFiles: ['./src/test/setup.ts'],
