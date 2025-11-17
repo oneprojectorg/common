a0c1c4166fecd0a6471eaaeb82e1edc11e64f968
Scott Cazan <scottcazan@gmail.com>
2025-11-17 15:37:49 +0100

    remove "convenience" export

--- apps/app/src/components/RichTextEditor/index.ts
+++ apps/app/src/components/RichTextEditor/index.ts
@@ -1,12 +1,8 @@
 // App-specific RichTextEditor with toolbar
-export { RichTextEditorWithToolbar, type RichTextEditorWithToolbarProps } from './RichTextEditorWithToolbar';
-
-// Re-export from @op/ui for convenience
 export {
-  RichTextEditor,
-  RichTextViewer,
-  type RichTextEditorRef,
-} from '@op/ui/RichTextEditor';
+  RichTextEditorWithToolbar,
+  type RichTextEditorWithToolbarProps,
+} from './RichTextEditorWithToolbar';
 
 // App-specific toolbar components
 export { RichTextEditorToolbar } from './RichTextEditorToolbar';
