d9321f7fb552bb686ff7b5e2f476effdda2fd3f5
Valentino Hudhra <v.hudhra@gmail.com>
2025-11-27 15:56:16 +0100

    Upgrade to Next.js 16

--- apps/api/next.config.mjs
+++ apps/api/next.config.mjs
@@ -27,17 +27,15 @@ dotenv.config({
 
 /** @type {import('next').NextConfig} */
 const config = {
-  experimental: {
-    serverComponentsExternalPackages: [
-      'sharp',
-      'onnxruntime-node',
-      'thread-stream',
-      'pino',
-      'pino-worker',
-      '@axiomhq/pino',
-    ],
-    instrumentationHook: true,
-  },
+  serverExternalPackages: [
+    'sharp',
+    'onnxruntime-node',
+    'thread-stream',
+    'pino',
+    'pino-worker',
+    '@axiomhq/pino',
+  ],
+  turbopack: {},
   webpack: (cfg) => {
     // Grab the existing rule that handles SVG imports
     const fileLoaderRule = cfg.module.rules.find((rule) =>
--- apps/api/proxy.ts
+++ apps/api/proxy.ts
@@ -11,7 +11,7 @@ const corsOptions = {
 
 const { IS_DEVELOPMENT } = OPURLConfig('API');
 
-export function middleware(request: NextRequest, event: NextFetchEvent) {
+export function proxy(request: NextRequest, event: NextFetchEvent) {
   logger.info(...transformMiddlewareRequest(request));
 
   event.waitUntil(logger.flush());
