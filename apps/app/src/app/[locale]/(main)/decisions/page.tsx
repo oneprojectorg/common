d9b6256bfbac398c280a8ca4a4ecee55c88b9f28
Scott Cazan <scottcazan@gmail.com>
2025-12-01 19:03:53 +0100

    Better error handling

--- apps/app/src/app/[locale]/(main)/decisions/page.tsx
+++ apps/app/src/app/[locale]/(main)/decisions/page.tsx
@@ -1,4 +1,4 @@
-import { ProcessStatus } from '@op/api/encoders';
+import { DecisionProfileList, ProcessStatus } from '@op/api/encoders';
 import { createClient } from '@op/api/serverClient';
 
 import { AllDecisions } from '@/components/decisions/AllDecisions';
@@ -10,37 +10,34 @@ import {
 export const dynamic = 'force-dynamic';
 
 const DecisionsListingPage = async () => {
+  let decisions: DecisionProfileList = {
+    items: [],
+    hasMore: false,
+    next: null,
+  };
+
   try {
     const client = await createClient();
-    const decisions = await client.decision.listDecisionProfiles({
+    decisions = await client.decision.listDecisionProfiles({
       limit: 20,
       status: ProcessStatus.PUBLISHED,
     });
-
-    return (
-      <ListPageLayout className="gap-4 pt-12 sm:gap-6 sm:pt-12">
-        <div className="flex flex-col gap-2">
-          <ListPageLayoutHeader>Decision-making processes</ListPageLayoutHeader>
-          <p className="text-neutral-charcoal">
-            Discover new ways to collectively decide together.
-          </p>
-        </div>
-        <AllDecisions initialData={decisions} />
-      </ListPageLayout>
-    );
   } catch (error) {
-    return (
-      <ListPageLayout className="gap-4 pt-12 sm:gap-8 sm:pt-12">
-        <div className="flex flex-col gap-2">
-          <ListPageLayoutHeader>Decision-making processes</ListPageLayoutHeader>
-          <p className="text-sm text-neutral-charcoal">
-            Discover new ways to collectively decide together.
-          </p>
-        </div>
-        <AllDecisions initialData={{ items: [], hasMore: false, next: null }} />
-      </ListPageLayout>
-    );
+    // log error but return the empty list
+    console.log(error);
   }
+
+  return (
+    <ListPageLayout className="gap-4 pt-8 sm:gap-6 sm:pt-12">
+      <div className="flex flex-col gap-2">
+        <ListPageLayoutHeader>Decision-making processes</ListPageLayoutHeader>
+        <p className="text-neutral-charcoal">
+          Discover new ways to collectively decide together.
+        </p>
+      </div>
+      <AllDecisions initialData={decisions} />
+    </ListPageLayout>
+  );
 };
 
 export default DecisionsListingPage;
