b1046c0e632cac842dde0458e6812fd86705bce2
Valentino Hudhra <v.hudhra@gmail.com>
2025-12-03 08:21:40 +0100

    encode the whole object

--- services/api/src/encoders/joinProfileRequests.ts
+++ services/api/src/encoders/joinProfileRequests.ts
@@ -4,14 +4,12 @@ import { z } from 'zod';
 
 import { baseProfileEncoder } from './profiles';
 
-export const joinProfileRequestEncoder = createSelectSchema(joinProfileRequests)
-  .pick({
-    status: true,
-  })
-  .extend({
-    requestProfile: baseProfileEncoder,
-    targetProfile: baseProfileEncoder,
-  });
+export const joinProfileRequestEncoder = createSelectSchema(
+  joinProfileRequests,
+).extend({
+  requestProfile: baseProfileEncoder,
+  targetProfile: baseProfileEncoder,
+});
 
 export type JoinProfileRequestResponse = z.infer<
   typeof joinProfileRequestEncoder
