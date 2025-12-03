5fa92cac90591aceefaf3dfccab827bf21910fcf
Valentino Hudhra <2587839+valentin0h@users.noreply.github.com>
2025-12-03 13:57:25 +0100

    Add request to join a profile - database schema (#355)
    Adds the database schema and migrations for the request to join profile
    feature.
    
    ---
    
    ### Stacked PRs
    
    This PR is part of a stacked PR series for the **Request to Join
    Profile** feature:
    
    | # | PR | Description |
    |---|---|---|
    | 1 | #355 | Database schema |
    | 2 | #356 | Add API |
    | 3 | #358 | Get API |
    | 4 | #359 | List API |
    | 5 | #362 | Update API |
    | 6 | #365 | List UI |
    | 7 | #366 | Request UI |

---
+++ services/db/schema/tables/joinProfileRequests.sql.ts
@@ -0,0 +1,69 @@
+import { relations } from 'drizzle-orm';
+import { index, pgEnum, pgTable, uniqueIndex, uuid } from 'drizzle-orm/pg-core';
+
+import {
+  autoId,
+  enumToPgEnum,
+  serviceRolePolicies,
+  timestamps,
+} from '../../helpers';
+import { profiles } from './profiles.sql';
+
+export enum JoinProfileRequestStatus {
+  PENDING = 'pending',
+  APPROVED = 'approved',
+  REJECTED = 'rejected',
+}
+
+export const joinProfileRequestsStatusEnum = pgEnum(
+  'join_profile_request_status',
+  enumToPgEnum(JoinProfileRequestStatus),
+);
+
+// Store requests to join profiles from other profiles
+export const joinProfileRequests = pgTable(
+  'profile_requests',
+  {
+    id: autoId().primaryKey(),
+    requestProfileId: uuid('request_profile_id')
+      .notNull()
+      .references(() => profiles.id, {
+        onDelete: 'cascade',
+      }),
+    targetProfileId: uuid('target_profile_id')
+      .notNull()
+      .references(() => profiles.id, {
+        onDelete: 'cascade',
+      }),
+    status: joinProfileRequestsStatusEnum('status')
+      .default(JoinProfileRequestStatus.PENDING)
+      .notNull(),
+    ...timestamps,
+  },
+  (table) => [
+    ...serviceRolePolicies,
+    index().on(table.requestProfileId),
+    index().on(table.targetProfileId),
+    uniqueIndex('requestToTarget_idx').on(
+      table.requestProfileId,
+      table.targetProfileId,
+    ),
+  ],
+);
+
+export const joinProfileRequestsRelations = relations(
+  joinProfileRequests,
+  ({ one }) => ({
+    requestProfile: one(profiles, {
+      fields: [joinProfileRequests.requestProfileId],
+      references: [profiles.id],
+    }),
+
+    targetProfile: one(profiles, {
+      fields: [joinProfileRequests.targetProfileId],
+      references: [profiles.id],
+    }),
+  }),
+);
+
+export type JoinProfileRequest = typeof joinProfileRequests.$inferSelect;
