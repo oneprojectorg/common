5fd87803880c94a3283ddd7cc39fde3b6353ea96
Valentino Hudhra <v.hudhra@gmail.com>
2025-12-15 10:39:27 +0100

    revert openapi fetch

--- apps/api/app/api/v1/[...trpc]/route.ts
+++ apps/api/app/api/v1/[...trpc]/route.ts
@@ -1,12 +1,7 @@
-import {
-  MUTATION_CHANNELS_HEADER,
-  SUBSCRIPTION_CHANNELS_HEADER,
-  appRouter,
-  createContext,
-} from '@op/api';
+import { appRouter, createContext } from '@op/api';
 import { API_OPENAPI_PATH } from '@op/core';
 import { createSBServerClient } from '@op/supabase/server';
-import { fetchRequestHandler } from '@trpc/server/adapters/fetch';
+import { createOpenApiFetchHandler } from 'trpc-to-openapi';
 
 import { verifyAdminOnly } from '../../../route';
 
@@ -16,35 +11,12 @@ const handler = async (req: Request) => {
 
   verifyAdminOnly(data);
 
-  return fetchRequestHandler({
+  // Handle incoming OpenAPI requests
+  return createOpenApiFetchHandler({
     endpoint: `/${API_OPENAPI_PATH}`,
-    req,
     router: appRouter,
     createContext,
-    responseMeta({ ctx }) {
-      if (!ctx) {
-        return {};
-      }
-
-      const headers: Record<string, string> = {};
-      const mutationChannels = ctx.getChannels('mutation');
-      const subscriptionChannels = ctx.getChannels('subscription');
-
-      if (subscriptionChannels.length > 0) {
-        headers[SUBSCRIPTION_CHANNELS_HEADER] = subscriptionChannels.join(',');
-      }
-      if (mutationChannels.length > 0) {
-        headers[MUTATION_CHANNELS_HEADER] = mutationChannels.join(',');
-      }
-
-      if (Object.keys(headers).length > 0) {
-        return {
-          headers: new Headers(Object.entries(headers)),
-        };
-      }
-
-      return {};
-    },
+    req,
   });
 };
 
