ee2efff964be3ddd4688b1f5658028ec1ca77558
Valentino Hudhra <v.hudhra@gmail.com>
2025-12-15 10:17:42 +0100

    update comments

--- services/api/src/middlewares/withMutationChannels.ts
+++ services/api/src/middlewares/withMutationChannels.ts
@@ -4,10 +4,9 @@ import type { MiddlewareBuilderBase } from '../types';
 
 /**
  * Middleware that sets mutation channels for realtime subscriptions.
- * The channels will be sent to the client via the x-mutation-channels header.
- * Used in mutations to notify which data changed.
  *
  * @param channels - Array of channel names to set for this mutation
+ * TODO: can I limit this to only mutation contexts?
  */
 const withMutationChannels = (channels: ChannelName[]) => {
   const withMutationChannelsInner: MiddlewareBuilderBase = async ({
--- services/api/src/middlewares/withSubscriptionChannels.ts
+++ services/api/src/middlewares/withSubscriptionChannels.ts
@@ -4,8 +4,6 @@ import type { MiddlewareBuilderBase } from '../types';
 
 /**
  * Middleware that sets subscription channels for query invalidation.
- * The channels will be sent to the client via the x-subscription-channels header.
- * Used in queries to declare which channels they subscribe to.
  *
  * @param channels - Array of channel names to subscribe to
  */
