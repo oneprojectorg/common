7b9bdec4bbc150ca3b2681272582754bef57b0b3
Nour Malaeb <nourmalaeb@gmail.com>
2025-12-17 13:52:43 -0500

    Speed up login link appearance

--- apps/app/src/components/screens/ComingSoon/ComingSoonScreen.tsx
+++ apps/app/src/components/screens/ComingSoon/ComingSoonScreen.tsx
@@ -25,12 +25,27 @@ export const ComingSoonScreen = () => {
         <div className="absolute top-0 z-20 h-20 w-full bg-gradient-to-b from-[black] via-[rgba(0,0,0,0.35)] via-40% sm:hidden" />
         <div className="absolute bottom-0 z-20 h-20 w-full bg-gradient-to-t from-[black] via-[rgba(0,0,0,0.35)] via-40% sm:hidden" />
       </div>
-      <div className="absolute inset-0 z-20 flex flex-col justify-between p-4 py-16 text-white sm:p-8 md:p-12 xl:p-20">
+      <div className="absolute inset-0 z-20 flex flex-col gap-6 p-4 py-16 text-white sm:p-8 md:p-12 xl:p-20">
+        {/* Render the sign in link first for screenreaders */}
+        <motion.p
+          className="order-3 sm:text-lg"
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition={{ duration: 1, delay: 0.5 }}
+        >
+          Already have an account?{' '}
+          <a
+            href="/login"
+            className="underline transition-opacity hover:opacity-80"
+          >
+            Sign in
+          </a>
+        </motion.p>
         <motion.h1
           initial={{ opacity: 0 }}
           animate={{ opacity: 1 }}
           transition={{ duration: 1, delay: 0.5 }}
-          className="font-serif text-3xl font-light leading-[1.1] sm:text-6xl sm:leading-[1.25]"
+          className="order-1 h-full font-serif text-3xl font-light leading-[1.1] sm:text-6xl sm:leading-[1.25]"
         >
           <em>Common.</em>{' '}
           <motion.span
@@ -42,23 +57,15 @@ export const ComingSoonScreen = () => {
             grow economic democracy to global scale.
           </motion.span>
         </motion.h1>
-        <motion.div
-          className="flex flex-col gap-6"
-          initial={{ opacity: 0 }}
-          animate={{ opacity: 1 }}
-          transition={{ duration: 1, delay: 3 }}
-        >
-          <WaitlistSignup />
-          <p className="sm:text-lg">
-            Already have an account?{' '}
-            <a
-              href="/login"
-              className="underline transition-opacity hover:opacity-80"
-            >
-              Sign in
-            </a>
-          </p>
-        </motion.div>
+        <div className="order-2 flex flex-col gap-6">
+          <motion.div
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{ duration: 1, delay: 3 }}
+          >
+            <WaitlistSignup />
+          </motion.div>
+        </div>
       </div>
     </div>
   );
