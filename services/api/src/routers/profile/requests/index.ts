bbdc7b88d1c8bce3c9fdf1f3bb4f6f2f864fe164
Valentino Hudhra <v.hudhra@gmail.com>
2025-12-17 08:59:49 +0100

    Add request to join profile cancellation

--- services/api/src/routers/profile/requests/index.ts
+++ services/api/src/routers/profile/requests/index.ts
@@ -1,11 +1,13 @@
 import { mergeRouters } from '../../../trpcFactory';
 import { createJoinRequestRouter } from './createJoinRequest';
+import { deleteJoinRequestRouter } from './deleteJoinRequest';
 import { getJoinRequestRouter } from './getJoinRequest';
 import { listJoinRequestsRouter } from './listJoinRequests';
 import { updateJoinRequestRouter } from './updateJoinRequest';
 
 const requestsRouter = mergeRouters(
   createJoinRequestRouter,
+  deleteJoinRequestRouter,
   getJoinRequestRouter,
   listJoinRequestsRouter,
   updateJoinRequestRouter,
