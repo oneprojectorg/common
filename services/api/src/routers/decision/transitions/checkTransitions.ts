3b6c686ea0556724f4ca89d9d40957ca5de19212
Scott Cazan <scottcazan@gmail.com>
2025-12-18 11:50:02 +0100

    Move transition system over to legacy encoders for the moment

--- services/api/src/routers/decision/transitions/checkTransitions.ts
+++ services/api/src/routers/decision/transitions/checkTransitions.ts
@@ -1,10 +1,6 @@
 import { NotFoundError, UnauthorizedError, checkTransitions } from '@op/common';
 import { TRPCError } from '@trpc/server';
-
-import {
-  checkTransitionInputSchema,
-  transitionCheckResultEncoder,
-} from '../../../encoders/decision';
+import { checkTransitionInputSchema, transitionCheckResultEncoder } from '../../../encoders/legacyDecision';
 import withAnalytics from '../../../middlewares/withAnalytics';
 import withAuthenticated from '../../../middlewares/withAuthenticated';
 import withRateLimited from '../../../middlewares/withRateLimited';
