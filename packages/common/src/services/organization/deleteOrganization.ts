056c81ee2aae7cfa31a29580fda7651e98d36494
Nour Malaeb <nourmalaeb@gmail.com>
2026-01-05 17:48:21 +0200

    Update assertAcces to use UPDATE instead of DELETE permission

--- packages/common/src/services/organization/deleteOrganization.ts
+++ packages/common/src/services/organization/deleteOrganization.ts
@@ -37,7 +37,7 @@ export async function deleteOrganization({
     throw new UnauthorizedError('You are not a member of this organization');
   }
 
-  assertAccess({ admin: permission.DELETE }, orgUser.roles || []);
+  assertAccess({ admin: permission.UPDATE }, orgUser.roles || []);
 
   // Delete the organization profile
   // The cascade delete will handle removing org data
