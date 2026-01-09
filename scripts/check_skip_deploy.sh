7468ab6672eef054e9cd20880837f2b518c6603a
Scott Cazan <scottcazan@gmail.com>
2026-01-09 16:39:06 +0100

    Remove skip-deploy exception

--- scripts/check_skip_deploy.sh
+++ scripts/check_skip_deploy.sh
@@ -3,7 +3,7 @@
 set -e
 
 # Execute the turbo-ignore command only if the current branch is "dev" or "main"
-if [[ "$VERCEL_GIT_COMMIT_REF" == "dev" || "$VERCEL_GIT_COMMIT_REF" == "main" || "$VERCEL_GIT_COMMIT_REF" == "log-debugging" ]]; then
+if [[ "$VERCEL_GIT_COMMIT_REF" == "dev" || "$VERCEL_GIT_COMMIT_REF" == "main" ]]; then
   npx turbo-ignore --fallback=HEAD^1
 else
   echo "Error: This command should only be run on 'dev' or 'main'. Current branch: $VERCEL_GIT_COMMIT_REF"
