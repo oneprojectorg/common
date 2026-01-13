3e250e5a9370fd2bcb5ff24b37d64775ad301fbc
Nour Malaeb <nourmalaeb@gmail.com>
2026-01-13 15:18:20 +0200

    Add constants to styles package

---
+++ packages/styles/constants.ts
@@ -0,0 +1,9 @@
+export const breakpoints = {
+  xxs: '320px', // 20rem
+  xs: '480px', // 30rem
+  sm: '640px', // 40rem
+  md: '768px', // 48rem
+  lg: '1024px', // 64rem
+  xl: '1280px', // 80rem
+  xxl: '1536px', // 96rem
+};
