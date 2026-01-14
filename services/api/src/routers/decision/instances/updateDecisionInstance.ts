a74a5be60e09621e29fdc2e4b48a4ffef85c574e
Scott Cazan <scottcazan@gmail.com>
2026-01-14 18:14:30 +0100

    remove metadata

--- services/api/src/routers/decision/instances/updateDecisionInstance.ts
+++ services/api/src/routers/decision/instances/updateDecisionInstance.ts
@@ -1,5 +1,4 @@
 import { updateDecisionInstance } from '@op/common';
-import type { OpenApiMeta } from 'trpc-to-openapi';
 
 import {
   decisionProfileEncoder,
@@ -10,23 +9,11 @@ import withAuthenticated from '../../../middlewares/withAuthenticated';
 import withRateLimited from '../../../middlewares/withRateLimited';
 import { loggedProcedure, router } from '../../../trpcFactory';
 
-const meta: OpenApiMeta = {
-  openapi: {
-    enabled: true,
-    method: 'PATCH',
-    path: '/decision/instance/{instanceId}',
-    protect: true,
-    tags: ['decision'],
-    summary: 'Update a decision process instance',
-  },
-};
-
 export const updateDecisionInstanceRouter = router({
   updateDecisionInstance: loggedProcedure
     .use(withRateLimited({ windowSize: 10, maxRequests: 10 }))
     .use(withAuthenticated)
     .use(withAnalytics)
-    .meta(meta)
     .input(updateDecisionInstanceInputSchema)
     .output(decisionProfileEncoder)
     .mutation(async ({ ctx, input }) => {
