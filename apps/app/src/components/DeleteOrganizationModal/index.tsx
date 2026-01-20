6fdffa4db85aecdf3580f52e066db48e0bd95771
Nour Malaeb <nourmalaeb@gmail.com>
2026-01-21 05:58:51 +0900

    Add org deletion UI (#385)

---
+++ apps/app/src/components/DeleteOrganizationModal/index.tsx
@@ -0,0 +1,315 @@
+'use client';
+
+import { getPublicUrl } from '@/utils';
+import { useUser } from '@/utils/UserProvider';
+import { RouterOutput } from '@op/api';
+import { trpc } from '@op/api/client';
+import { EntityType } from '@op/api/encoders';
+import { match } from '@op/core';
+import { Avatar } from '@op/ui/Avatar';
+import { Button } from '@op/ui/Button';
+import { Modal, ModalFooter, ModalHeader } from '@op/ui/Modal';
+import { Radio, RadioGroup } from '@op/ui/RadioGroup';
+import { toast } from '@op/ui/Toast';
+import Image from 'next/image';
+import { useRouter } from 'next/navigation';
+import { useEffect, useState, useTransition } from 'react';
+
+import { useTranslations } from '@/lib/i18n';
+
+// TODO: typing here needs to be fixed
+type AccountProfile = RouterOutput['account']['getUserProfiles'][number];
+
+interface OrgDeletionModalProps {
+  isOpen: boolean;
+  onOpenChange?: (isOpen: boolean) => void;
+}
+
+export const DeleteOrganizationModal = ({
+  isOpen,
+  onOpenChange,
+}: OrgDeletionModalProps) => {
+  const t = useTranslations();
+  const { data: profiles } = trpc.account.getUserProfiles.useQuery();
+  const [selectedProfileId, setSelectedProfileId] = useState<string>();
+  const [profileToDelete, setProfileToDelete] = useState<AccountProfile>();
+  const [currentStep, setCurrentStep] = useState(0);
+
+  const [isSubmitting, startTransition] = useTransition();
+  const utils = trpc.useUtils();
+  const { user } = useUser();
+  const deleteProfile = trpc.organization.deleteOrganization.useMutation();
+  const switchProfile = trpc.account.switchProfile.useMutation();
+
+  const router = useRouter();
+
+  const userProfiles =
+    profiles?.filter(
+      (profile) =>
+        // Filter out everything that's not an ORG profile
+        profile && profile?.type === EntityType.ORG,
+    ) ?? [];
+
+  const closeModal = () => {
+    onOpenChange?.(false);
+    setProfileToDelete(undefined);
+    setCurrentStep(0);
+  };
+
+  const handleSubmit = () => {
+    if (!selectedProfileId) {
+      throw new Error('handleSubmit called without selectedProfileId');
+    }
+    startTransition(async () => {
+      try {
+        await deleteProfile.mutateAsync({
+          organizationProfileId: selectedProfileId,
+        });
+
+        // Check if the deletedProfile is the user's current profile
+        // if so, switch to the user's individual profile
+        if (user.currentProfile?.id === selectedProfileId) {
+          const personalProfile = profiles?.find(
+            (profile) => profile.type === EntityType.INDIVIDUAL,
+          );
+          if (personalProfile) {
+            await switchProfile.mutateAsync({ profileId: personalProfile.id });
+          }
+        }
+
+        await utils.account.invalidate();
+        await utils.organization.listAllPosts.invalidate();
+
+        router.refresh();
+        setCurrentStep(2);
+      } catch (error) {
+        toast.error({ message: t('Failed to delete account') });
+      }
+    });
+  };
+
+  // Clear selections when closing modal
+  useEffect(() => {
+    if (!isOpen) {
+      setSelectedProfileId(undefined);
+      setProfileToDelete(undefined);
+      setCurrentStep(0);
+    }
+  }, [isOpen]);
+
+  const steps = [
+    <SelectProfileStep
+      key="select"
+      allProfiles={userProfiles}
+      selectedProfile={selectedProfileId}
+      setSelectedProfile={setSelectedProfileId}
+      cancelButtonAction={closeModal}
+      submitButtonAction={() => {
+        setProfileToDelete(
+          userProfiles.find((profile) => profile.id === selectedProfileId),
+        );
+        setCurrentStep(1);
+      }}
+    />,
+    profileToDelete && (
+      <ConfirmProfileStep
+        key="confirm"
+        submitButtonAction={handleSubmit}
+        backButtonAction={() => setCurrentStep(0)}
+        profileToDelete={profileToDelete}
+        isSubmitting={isSubmitting}
+      />
+    ),
+    profileToDelete && (
+      <SuccessStep
+        key="success"
+        submitButtonAction={closeModal}
+        deletedProfileName={profileToDelete.name}
+      />
+    ),
+  ];
+
+  return (
+    <Modal
+      isOpen={isOpen}
+      onOpenChange={onOpenChange}
+      isDismissable
+      className="text-left"
+    >
+      {steps[currentStep]}
+    </Modal>
+  );
+};
+
+const SelectProfileStep = ({
+  allProfiles,
+  selectedProfile,
+  setSelectedProfile,
+  cancelButtonAction,
+  submitButtonAction,
+}: {
+  allProfiles: AccountProfile[];
+  selectedProfile?: string;
+  setSelectedProfile: (profiles: string) => void;
+  cancelButtonAction: () => void;
+  submitButtonAction: () => void;
+}) => {
+  const t = useTranslations();
+  return (
+    <>
+      <ModalHeader>{t('Delete an Account')}</ModalHeader>
+      <div className="flex flex-col gap-4 px-6 py-4">
+        <p id="select-accounts-label">
+          {t(
+            'Please select the account you’d like to delete. This action cannot be undone.',
+          )}
+        </p>
+        <RadioGroup
+          aria-labelledby="select-accounts-label"
+          value={selectedProfile}
+          onChange={setSelectedProfile}
+        >
+          {allProfiles.map((profile) => {
+            const profileType = match(profile.type, {
+              org: t('Organization'),
+              individual: t('Individual'),
+            });
+            return (
+              <Radio
+                key={profile.id}
+                className="items-start py-2"
+                value={profile.id}
+              >
+                <div className="flex flex-col gap-0.5">
+                  <span className="text-base leading-[1.05] text-neutral-charcoal">
+                    {profile.name}
+                  </span>
+                  <p className="text-neutral-gray4">{profileType}</p>
+                </div>
+              </Radio>
+            );
+          })}
+        </RadioGroup>
+      </div>
+      <ModalFooter>
+        <Button
+          color="neutral"
+          onPress={cancelButtonAction}
+          className="w-full sm:w-auto"
+        >
+          {t('Cancel')}
+        </Button>
+        <Button
+          color="destructive"
+          type="button"
+          onPress={submitButtonAction}
+          isDisabled={!selectedProfile}
+          className="w-full sm:w-auto"
+        >
+          {t('Remove')}
+        </Button>
+      </ModalFooter>
+    </>
+  );
+};
+
+const ConfirmProfileStep = ({
+  submitButtonAction,
+  backButtonAction,
+  profileToDelete,
+  isSubmitting,
+}: {
+  profileToDelete: AccountProfile;
+  backButtonAction: () => void;
+  submitButtonAction: () => void;
+  isSubmitting: boolean;
+}) => {
+  const t = useTranslations();
+  const profileType = match(profileToDelete.type, {
+    org: t('Organization'),
+    individual: t('Individual'),
+  });
+  const avatarUrl = profileToDelete.avatarImage?.name;
+  return (
+    <>
+      <ModalHeader>{t('Delete an Account')}</ModalHeader>
+      <div className="flex flex-col gap-2 px-6 py-4">
+        <p>
+          {t(
+            'You are about to delete this account. This action cannot be undone.',
+          )}
+        </p>
+        <div className="flex gap-2 rounded border border-red-100 p-4">
+          <Avatar
+            className="size-8 shrink-0"
+            placeholder={profileToDelete.name ?? ''}
+          >
+            {avatarUrl ? (
+              <Image
+                src={getPublicUrl(avatarUrl) ?? ''}
+                fill
+                className="object-cover"
+                alt={profileToDelete.name ?? t('User avatar')}
+              />
+            ) : null}
+          </Avatar>
+          <div className="flex flex-col">
+            <p className="font-medium">{profileToDelete.name}</p>
+            <p className="text-sm text-neutral-charcoal">{profileType}</p>
+          </div>
+        </div>
+      </div>
+      <ModalFooter>
+        <Button
+          className="w-full sm:w-auto"
+          color="neutral"
+          onPress={backButtonAction}
+        >
+          {t('Back')}
+        </Button>
+        <Button
+          className="w-full sm:w-auto"
+          color="destructive"
+          type="button"
+          onPress={submitButtonAction}
+          isPending={isSubmitting}
+          isDisabled={isSubmitting}
+        >
+          {isSubmitting ? t('Removing...') : t('Remove')}
+        </Button>
+      </ModalFooter>
+    </>
+  );
+};
+
+const SuccessStep = ({
+  deletedProfileName,
+  submitButtonAction,
+}: {
+  deletedProfileName: string;
+  submitButtonAction: () => void;
+}) => {
+  const t = useTranslations();
+  return (
+    <>
+      <ModalHeader>{t('Account Deleted')}</ModalHeader>
+      <div className="flex flex-col gap-2 px-6 py-4">
+        <p>
+          {t(
+            '{profileName} has been deleted. All associated data have been permanently removed.',
+            { profileName: deletedProfileName },
+          )}
+        </p>
+      </div>
+      <ModalFooter>
+        <Button
+          className="w-full sm:w-auto"
+          type="button"
+          onPress={submitButtonAction}
+        >
+          {t('Done')}
+        </Button>
+      </ModalFooter>
+    </>
+  );
+};
