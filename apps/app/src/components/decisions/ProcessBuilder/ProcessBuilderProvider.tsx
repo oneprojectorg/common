5a4f201854510efdb45b466de10403fd3be51220
Nour Malaeb <nourmalaeb@gmail.com>
2026-01-21 11:30:46 -0500

    Use visibilityConfig in ProcessBuilderProvider

--- apps/app/src/components/decisions/ProcessBuilder/ProcessBuilderProvider.tsx
+++ apps/app/src/components/decisions/ProcessBuilder/ProcessBuilderProvider.tsx
@@ -1,15 +1,35 @@
 'use client';
 
 import { SidebarProvider } from '@op/ui/Sidebar';
+import { createContext, useContext } from 'react';
+
+import {
+  DEFAULT_VISIBILITY_CONFIG,
+  type VisibilityConfig,
+} from './navigation-config';
+
+interface ProcessBuilderContextValue {
+  visibilityConfig: VisibilityConfig;
+}
+
+const ProcessBuilderContext = createContext<ProcessBuilderContextValue>({
+  visibilityConfig: DEFAULT_VISIBILITY_CONFIG,
+});
+
+export const useProcessBuilderContext = () => useContext(ProcessBuilderContext);
 
 export const ProcessBuilderProvider = ({
   children,
+  visibilityConfig = DEFAULT_VISIBILITY_CONFIG,
 }: {
   children: React.ReactNode;
+  visibilityConfig?: VisibilityConfig;
 }) => {
   return (
-    <SidebarProvider isOpen className="flex h-full flex-col">
-      {children}
-    </SidebarProvider>
+    <ProcessBuilderContext.Provider value={{ visibilityConfig }}>
+      <SidebarProvider isOpen className="flex h-full flex-col">
+        {children}
+      </SidebarProvider>
+    </ProcessBuilderContext.Provider>
   );
 };
