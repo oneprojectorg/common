afa143a0f057ceaf880469c2a45d2a22cef47242
Scott Cazan <scottcazan@gmail.com>
2026-01-21 15:03:14 +0100

    inline output encoder

--- services/api/src/routers/profile/users/removeUser.ts
+++ services/api/src/routers/profile/users/removeUser.ts
@@ -3,30 +3,28 @@ import { z } from 'zod';
 
 import { commonAuthedProcedure, router } from '../../../trpcFactory';
 
-const outputSchema = z.object({
-  id: z.string(),
-  authUserId: z.string(),
-  name: z.string().nullable(),
-  email: z.string(),
-  about: z.string().nullable(),
-  profileId: z.string(),
-  createdAt: z.union([z.string(), z.date()]).nullable(),
-  updatedAt: z.union([z.string(), z.date()]).nullable(),
-});
-
 export const removeUserRouter = router({
   removeUser: commonAuthedProcedure()
     .input(z.object({ profileUserId: z.uuid() }))
-    .output(outputSchema)
+    .output(
+      z.object({
+        id: z.string(),
+        authUserId: z.string(),
+        name: z.string().nullable(),
+        email: z.string(),
+        about: z.string().nullable(),
+        profileId: z.string(),
+        createdAt: z.union([z.string(), z.date()]).nullable(),
+        updatedAt: z.union([z.string(), z.date()]).nullable(),
+      }),
+    )
     .mutation(async ({ ctx, input }) => {
       const { user } = ctx;
       const { profileUserId } = input;
 
-      const result = await removeProfileUser({
+      return removeProfileUser({
         profileUserId,
         user,
       });
-
-      return outputSchema.parse(result);
     }),
 });
