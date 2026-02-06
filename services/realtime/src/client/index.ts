c440aa7f714f1eb55cb2dafbc403bfb20d0d4809
Valentino Hudhra <v.hudhra@gmail.com>
2026-02-06 15:26:51 +0100

    Replace centrifugo with Supabase broadcast

--- services/realtime/src/client/index.ts
+++ services/realtime/src/client/index.ts
@@ -1,2 +1,2 @@
-export { type RealtimeConfig, RealtimeManager } from './manager';
+export { type RealtimeConfig, type RealtimeHandler, RealtimeManager } from './manager';
 export type { RealtimeMessage } from '../schemas';
