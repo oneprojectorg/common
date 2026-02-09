8b2c95a3a930a94a656abe96d0bb60d65637ab40
Scott Cazan <scottcazan@gmail.com>
2026-02-09 12:14:41 +0100

    Add in org check for older org permissions

--- packages/common/src/services/decision/listProposals.ts
+++ packages/common/src/services/decision/listProposals.ts
@@ -15,7 +15,11 @@ import { assertAccess, checkPermission, permission } from 'access-zones';
 import { count as countFn } from 'drizzle-orm';
 
 import { UnauthorizedError } from '../../utils';
-import { getCurrentProfileId, getProfileAccessUser } from '../access';
+import {
+  assertInstanceProfileAccess,
+  getCurrentProfileId,
+  getProfileAccessUser,
+} from '../access';
 import { getProposalDocumentsContent } from './getProposalDocumentsContent';
 import { parseProposalData } from './proposalDataSchema';
 
@@ -86,6 +90,7 @@ export const listProposals = async ({
   const instance = await db
     .select({
       profileId: processInstances.profileId,
+      ownerProfileId: processInstances.ownerProfileId,
     })
     .from(processInstances)
     .where(eq(processInstances.id, processInstanceId))
@@ -106,7 +111,12 @@ export const listProposals = async ({
       profileId: instance[0].profileId,
     });
 
-    assertAccess({ profile: permission.READ }, profileUser?.roles ?? []);
+    assertInstanceProfileAccess({
+      user,
+      instance: instance[0],
+      profilePermissions: { profile: permission.READ },
+      orgFallbackPermissions: { decisions: permission.READ },
+    });
 
     // Check if user can manage proposals (approve/reject)
     canManageProposals = checkPermission(
