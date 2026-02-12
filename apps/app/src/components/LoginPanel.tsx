25092c1c7e10604cfaabdf6af5ac70df2a09952e
Scott Cazan <scottcazan@gmail.com>
2026-02-12 14:37:05 +0100

    format

--- apps/app/src/components/LoginPanel.tsx
+++ apps/app/src/components/LoginPanel.tsx
@@ -80,7 +80,11 @@ export const LoginPanel = () => {
   const handleLogin = async () => {
     const callbackUrl = new URL('/api/auth/callback', location.origin);
 
-    if (redirectParam && redirectParam.startsWith('/') && !redirectParam.startsWith('//')) {
+    if (
+      redirectParam &&
+      redirectParam.startsWith('/') &&
+      !redirectParam.startsWith('//')
+    ) {
       callbackUrl.searchParams.set('redirect', redirectParam);
     }
 
@@ -129,7 +133,11 @@ export const LoginPanel = () => {
     });
 
     if (data.user && data.session && data.user.role === 'authenticated') {
-      if (redirectParam && redirectParam.startsWith('/') && !redirectParam.startsWith('//')) {
+      if (
+        redirectParam &&
+        redirectParam.startsWith('/') &&
+        !redirectParam.startsWith('//')
+      ) {
         window.location.href = redirectParam;
       } else {
         window.location.reload();
