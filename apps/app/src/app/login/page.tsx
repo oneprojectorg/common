a08cc4b40a7dc91196ecfc23e921f92a07ec37ab
Scott Cazan <scottcazan@gmail.com>
2026-02-20 17:21:43 +0100

    More robust redirect

--- apps/app/src/app/login/page.tsx
+++ apps/app/src/app/login/page.tsx
@@ -2,32 +2,42 @@
 
 import { isSafeRedirectPath } from '@op/common/client';
 import { useAuthUser } from '@op/hooks';
-import { redirect, useSearchParams } from 'next/navigation';
+import { useRouter, useSearchParams } from 'next/navigation';
+import { useEffect } from 'react';
 
 import { LoginPanel } from '@/components/LoginPanel';
 
-const LoginPageWithLayout = () => {
-  return <LoginPanel />;
-};
-
 const LoginPage = () => {
   const user = useAuthUser();
+  const router = useRouter();
   const searchParams = useSearchParams();
   const redirectParam = searchParams.get('redirect');
+
+  useEffect(() => {
+    if (!user || user.isFetching || user.isPending) {
+      return;
+    }
+
+    if (user.isFetchedAfterMount && !user.data?.user) {
+      return;
+    }
+
+    if (isSafeRedirectPath(redirectParam)) {
+      router.replace(redirectParam);
+    } else {
+      router.replace('/');
+    }
+  }, [user, redirectParam, router]);
 
   if (!user || user.isFetching || user.isPending) {
     return null;
   }
 
   if (user.isFetchedAfterMount && !user.isFetching && !user.data?.user) {
-    return <LoginPageWithLayout />;
+    return <LoginPanel />;
   }
 
-  if (isSafeRedirectPath(redirectParam)) {
-    redirect(redirectParam);
-  }
-
-  redirect('/');
+  return null;
 };
 
 export default LoginPage;
