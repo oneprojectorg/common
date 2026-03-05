9351e9f610e5437e9335228e09ba8b3d8dc50e44
Scott Cazan <scottcazan@gmail.com>
2026-03-05 23:43:16 +0100

    Use non-scaling strokes

--- apps/app/src/components/IconProvider.tsx
+++ apps/app/src/components/IconProvider.tsx
@@ -4,7 +4,9 @@ import { IconContext } from 'react-icons';
 
 export const IconProvider = ({ children }: { children: React.ReactNode }) => {
   return (
-    <IconContext.Provider value={{ className: 'stroke-1' }}>
+    <IconContext.Provider
+      value={{ className: 'stroke-1 [&_*]:[vector-effect:non-scaling-stroke]' }}
+    >
       {children}
     </IconContext.Provider>
   );
