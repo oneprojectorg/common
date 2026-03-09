1d6860948eb96d9ba81e66bf27e0fe89499fdf68
Scott Cazan <scottcazan@gmail.com>
2026-03-09 23:40:57 +0100

    Updating e2e test for notifications (#736)
    Fixes an e2e test that was failing after adding in notified_at
    
    ---------
    
    Co-authored-by: Valentino Hudhra <v.hudhra@gmail.com>

--- tests/e2e/tests/onboarding.spec.ts
+++ tests/e2e/tests/onboarding.spec.ts
@@ -76,6 +76,7 @@ test.describe('Onboarding', () => {
       profileEntityType: EntityType.DECISION,
       accessRoleId: ROLES.MEMBER.id,
       invitedBy: instance.profileId,
+      notifiedAt: new Date().toISOString(),
     });
 
     await authenticateAsUser(page, {
