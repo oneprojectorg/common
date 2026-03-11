83b5be48bc46ee54257a3cd5d266ede2209a5b3e
Scott Cazan <scottcazan@gmail.com>
2026-03-11 13:14:33 +0100

    move handler

--- apps/app/src/components/decisions/ProfileInviteModal.tsx
+++ apps/app/src/components/decisions/ProfileInviteModal.tsx
@@ -471,15 +471,12 @@ function ProfileInviteModalContent({
         )}
 
         {/* Search Input */}
-        <div
-          ref={searchContainerRef}
-          onPaste={handlePaste}
-          onKeyDown={handleKeyDown}
-        >
+        <div ref={searchContainerRef} onPaste={handlePaste}>
           <SearchField
             placeholder={t('Search by name or email...')}
             value={searchQuery}
             onChange={setSearchQuery}
+            onKeyDown={handleKeyDown}
             className="w-full"
           />
         </div>
