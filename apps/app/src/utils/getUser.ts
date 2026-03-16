f6cc783f09fc2451dfd2010d751317399772dc04
Valentino Hudhra <v.hudhra@gmail.com>
2026-03-16 09:57:17 +0100

    revert getUser

--- apps/app/src/utils/getUser.ts
+++ apps/app/src/utils/getUser.ts
@@ -1,14 +1,10 @@
 import { createClient } from '@op/api/serverClient';
 import { cache } from 'react';
 
-const IS_E2E = process.env.E2E === 'true';
-
 /**
  * Cached user fetch for server components.
  */
-const getUserInner = async () => {
+export const getUser = cache(async () => {
   const client = await createClient();
   return client.account.getMyAccount();
-};
-
-export const getUser = IS_E2E ? getUserInner : cache(getUserInner);
+});
