b4a3b94eff8580fc62d8e562052f6f18f0af73d4
Scott Cazan <scottcazan@gmail.com>
2026-04-01 18:07:41 +0200

    fix: Invalidate user cache after completing onboarding

--- services/api/src/routers/account/completeOnboarding.ts
+++ services/api/src/routers/account/completeOnboarding.ts
@@ -1,3 +1,4 @@
+import { invalidate } from '@op/cache';
 import { completeOnboarding as completeOnboardingService } from '@op/common';
 
 import { commonAuthedProcedure, router } from '../../trpcFactory';
@@ -6,6 +7,11 @@ export const completeOnboarding = router({
   completeOnboarding: commonAuthedProcedure().mutation(async ({ ctx }) => {
     await completeOnboardingService({
       authUserId: ctx.user.id,
+    });
+
+    await invalidate({
+      type: 'user',
+      params: [ctx.user.id],
     });
   }),
 });
