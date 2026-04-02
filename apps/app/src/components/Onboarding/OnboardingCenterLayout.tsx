5a7463e5c126108ccfacd7059f06215a8b0b90c3
Scott Cazan <scottcazan@gmail.com>
2026-04-02 16:12:24 +0200

    Add translations, update px value

--- apps/app/src/components/Onboarding/OnboardingCenterLayout.tsx
+++ apps/app/src/components/Onboarding/OnboardingCenterLayout.tsx
@@ -12,7 +12,7 @@ export const OnboardingCenterLayout = ({
 }) => {
   return (
     <div className="flex h-[calc(100dvh-80px)] w-full items-center justify-center">
-      <div className="flex w-full max-w-[472px] flex-col items-center gap-8 px-4 sm:px-0">
+      <div className="flex w-full max-w-lg flex-col items-center gap-8 px-4 sm:px-0">
         <div className="flex flex-col gap-2 text-center">
           <Header1 className="text-neutral-black">{title}</Header1>
           <p className="text-sm leading-normal text-neutral-gray4">
