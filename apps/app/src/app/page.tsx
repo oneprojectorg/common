302fff0e5783271c1a74097a1ad10a9d77a3287c
Scott Cazan <scottcazan@gmail.com>
2026-04-03 17:30:37 +0200

    Fix: Don't use stale cache when onboarding (#877)
    Uses isFetching since stale cached data would be used even though stale
    is set to 0 which only triggers an immediate refetch.

--- apps/app/src/app/page.tsx
+++ apps/app/src/app/page.tsx
@@ -9,9 +9,9 @@ import { ComingSoonScreen } from '@/components/screens/ComingSoon/ComingSoonScre
 const MainPage = () => {
   const router = useRouter();
   const authUser = useAuthUser();
-  const { data: account, isPending } = trpc.account.getMyAccount.useQuery();
+  const { data: account, isFetching } = trpc.account.getMyAccount.useQuery();
 
-  if (authUser?.data && !isPending) {
+  if (authUser?.data && !isFetching) {
     if (authUser.data.user === null) {
       return <ComingSoonScreen />;
     }
