efd88b39dd0a6d1550b24e0c1d04ffc09f7c1e6a
Scott Cazan <scottcazan@gmail.com>
2026-04-15 22:38:52 +0200

    Remove concurrent

--- services/db/migrations/20260415202855_organic_abomination/migration.sql
+++ services/db/migrations/20260415202855_organic_abomination/migration.sql
@@ -1,2 +1,2 @@
-CREATE INDEX CONCURRENTLY "profile_rel_source_type_idx" ON "profile_relationships" ("source_profile_id","relationship_type");--> statement-breakpoint
-CREATE INDEX CONCURRENTLY "profile_rel_target_type_idx" ON "profile_relationships" ("target_profile_id","relationship_type");
+CREATE INDEX "profile_rel_source_type_idx" ON "profile_relationships" ("source_profile_id","relationship_type");--> statement-breakpoint
+CREATE INDEX "profile_rel_target_type_idx" ON "profile_relationships" ("target_profile_id","relationship_type");
