3fb7aa32fb284421b14007cc2868e7910e0bc99e
Scott Cazan <scottcazan@gmail.com>
2026-04-22 15:09:52 +0200

    Remove dead code

--- apps/app/src/components/Organizations/types.ts
+++
@@ -1,6 +0,0 @@
-import { Organization } from '@op/api/encoders';
-
-export type OrganizationListResponse = {
-  items: Array<Organization>;
-  next?: string | null;
-};
--- apps/app/src/components/form/multiStep/multi-step-context.ts
+++
@@ -1,5 +0,0 @@
-import { createContext } from 'react';
-
-import type { MultiStepValue } from './multi-step-value';
-
-export const MultiStepContext = createContext<MultiStepValue | null>(null);
