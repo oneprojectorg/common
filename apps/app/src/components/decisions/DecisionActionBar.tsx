0d0d2ef0e14d999d27f1e7f7fca5d2b4f75838c2
Scott Cazan <scottcazan@gmail.com>
2026-06-11 21:39:01 +0200

    Increase width

--- apps/app/src/components/decisions/DecisionActionBar.tsx
+++ apps/app/src/components/decisions/DecisionActionBar.tsx
@@ -64,7 +64,7 @@ export const DecisionActionBar = ({
               {label ?? t('Learn more')}
             </Button>
 
-            <Modal isDismissable className="sm:max-w-2xl">
+            <Modal isDismissable className="sm:max-w-3xl">
               <Dialog>
                 <ModalHeader>{label ?? t('About the process')}</ModalHeader>
                 <ModalBody>
