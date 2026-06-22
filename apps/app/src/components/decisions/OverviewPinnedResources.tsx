be60222069c28cf5dd0b47f6df5e50734552dea1
Nour Malaeb <nourmalaeb@gmail.com>
2026-06-22 17:57:57 -0400

    tweak

--- apps/app/src/components/decisions/OverviewPinnedResources.tsx
+++ apps/app/src/components/decisions/OverviewPinnedResources.tsx
@@ -79,7 +79,7 @@ export const PinnedResourcesError = () => {
         <Header3 className="font-sans text-sm text-neutral-gray4">
           {t('Pinned Resources')}
         </Header3>
-        <p className="text-sm text-neutral-charcoal">
+        <p className="text-neutral-charcoal">
           {t("Couldn't load pinned resources.")}
         </p>
       </section>
