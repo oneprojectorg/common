8f2982745bb1184d3a3435f27313871b6910113b
Nour Malaeb <nourmalaeb@gmail.com>
2026-07-15 19:39:59 -0400

    fix(sense): select and combobox separators use bg-input token

--- packages/sense/src/components/ui/select.tsx
+++ packages/sense/src/components/ui/select.tsx
@@ -152,7 +152,7 @@ function SelectSeparator({
   return (
     <SelectPrimitive.Separator
       data-slot="select-separator"
-      className={cn('pointer-events-none -mx-1 my-1 h-px bg-border', className)}
+      className={cn('pointer-events-none -mx-1 my-1 h-px bg-input', className)}
       {...props}
     />
   );
