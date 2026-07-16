ddf0a41fe77e3eabb75a041bfb15f8b878a2ccfc
Nour Malaeb <nourmalaeb@gmail.com>
2026-07-15 22:17:51 -0400

    fix(sense): select and combobox menu separators use default border color

--- packages/sense/src/components/ui/combobox.tsx
+++ packages/sense/src/components/ui/combobox.tsx
@@ -213,7 +213,7 @@ function ComboboxSeparator({
   return (
     <ComboboxPrimitive.Separator
       data-slot="combobox-separator"
-      className={cn('-mx-1 my-1 h-px bg-input', className)}
+      className={cn('-mx-1 my-1 h-px bg-border', className)}
       {...props}
     />
   );
